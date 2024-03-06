e72a5b121010715bbb83ff986896037248dc2e19
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-06 09:26:28 +0100

    Dodanie podstawowych zapytań SQL

--- sql/podstawy.sql
+++ sql/podstawy.sql
@@ -1,1 +1,27 @@
 
+-- Wybór bazy danych
+USE sakila
+
+-- SELECT {kolumny} FROM {schemat.tabela}
+
+-- SELECT first_name, last_name, email, active FROM dbo.customer 
+
+-- SELECT {kolumny} FROM {tabela} schemat jest domyœlny (dbo)
+
+SELECT first_name, last_name, email, active FROM customer 
+
+-- Wyœwietl listê aktorów (imiê i nazwisko)
+SELECT first_name, last_name FROM dbo.actor
+
+-- Wyœwietl wszystkie kolumny
+SELECT * FROM customer
+
+-- Ograniczenie iloœci rekordów
+SELECT TOP(100) * FROM customer
+
+-- Wyœwietl 20 aktorów (imiê i nazwisko)
+-- | first_name | last_name |
+
+
+
+
