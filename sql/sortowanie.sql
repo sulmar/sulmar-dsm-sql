ad7b192b7ec1f9e4a92d101f826620b180ce589a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-06 13:22:49 +0100

    Dodanie sortowania

--- sql/sortowanie.sql
+++ sql/sortowanie.sql
@@ -1,1 +1,7 @@
+-- Porz¹dkowanie zbioru na podstawie kolumny
+SELECT * FROM customer
+ORDER BY first_name
 
+-- Porz¹dkowanie zbioru na podstawie kilku kolumn
+SELECT * FROM customer
+ORDER BY first_name, last_name
