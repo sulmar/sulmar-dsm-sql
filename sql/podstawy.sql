f5c25b93fd5d8676a93956b3c5d9318a8eb70381
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-06 09:40:09 +0100

    Dodanie aliasów kolumn

--- sql/podstawy.sql
+++ sql/podstawy.sql
@@ -30,6 +30,21 @@ SELECT DISTINCT first_name FROM customer
 -- Wyœwietl unikalne imiona aktorów. Ile ich jest?
 SELECT DISTINCT first_name FROM actor
 
+-- Aliasy kolumn
+SELECT 
+	first_name AS imie, 
+	last_name AS nazwisko, 
+	email AS [adres email] 
+FROM customer
+
+-- Wyœwietl aktorów (imiê i nazwisko)
+-- | imiê aktora | nazwisko aktora |
+
+SELECT 
+	first_name AS [imiê aktora], 
+	last_name AS [nazwisko aktora] 
+FROM actor
+
 
 
 
