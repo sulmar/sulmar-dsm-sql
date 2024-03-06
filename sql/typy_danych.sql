ebd5d471f101531bcb4301eafa7ec7546eab76e3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-06 14:05:06 +0100

    Dodanie typów danych

---
+++ sql/typy_danych.sql
@@ -0,0 +1,27 @@
+
+-- int - liczba ca³kowita 
+-- tinyint - liczba ca³kowita
+
+-- decimal - liczba rzeczywista
+-- numeric - liczba rzeczywista
+
+-- float - liczba zmienno-przecinkowa
+-- real  - liczba zmienno-przecinkowa
+
+
+-- varchar(45) - tekst o zmiennej d³ugoœci 
+-- char        - tekst o sta³ej d³ugoœci
+-- char(10)
+
+-- datetime		- data i czas
+
+-- bit		- flaga bitowe 1 - true 0 - false
+
+
+
+
+
+
+
+
+
