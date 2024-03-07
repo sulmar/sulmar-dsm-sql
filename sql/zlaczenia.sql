d84593f805c25afa9b44befa1379e4127e671703
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-07 09:08:42 +0100

    Dodanie złączenia inner join

--- sql/zlaczenia.sql
+++ sql/zlaczenia.sql
@@ -1,1 +1,11 @@
 
+SELECT 
+	first_name, 
+	last_name, 	
+	address.address,
+	address.postal_code
+FROM 
+	customer
+INNER JOIN -- typ z³¹czenia
+	address 
+		ON customer.address_id = address.address_id -- warunek z³¹czenia
