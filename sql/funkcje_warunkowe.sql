4d6787480531ce0570c36eb50d0edbf3a41a983b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-08 08:20:01 +0100

    Dodanie rozwiązania

--- sql/funkcje_warunkowe.sql
+++ sql/funkcje_warunkowe.sql
@@ -24,3 +24,31 @@ FROM
 	film
 
 
+SELECT 
+	first_name, 
+	last_name,  
+	CASE active	
+		WHEN 1 THEN 'Aktywny'
+		WHEN 0 THEN 'Nieaktywny'
+	END AS active
+FROM customer
+
+-- Wyœwietl dzieñ tygodnia w jêzyku polskim w którym wypo¿yczono film 
+-- bez u¿ycia dodatkowej funkcji
+
+SELECT		
+	CASE datepart(weekday, rental_date)
+		WHEN 1 THEN 'PONIEDZIA£EK'
+		WHEN 2 THEN 'WTOREK'
+		WHEN 3 THEN 'ŒRODA'
+		WHEN 4 THEN 'CZWARTEK'
+		WHEN 5 THEN 'PIATEK'
+		WHEN 6 THEN 'SOBOTA'
+		WHEN 7 THEN 'NIEDZIELA'
+	end as dni_tygodnia,
+	count(*) as ile_wypo¿yczono
+FROM rental
+GROUP BY datepart(weekday, rental_date)
+ORDER BY ile_wypo¿yczono desc
+
+set language polish
