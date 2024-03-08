309b90bd390da2f0c65a109d47e787537c5004df
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-08 11:38:35 +0100

    Dodanie zadania do parametrów

--- sql/parametry.sql
+++ sql/parametry.sql
@@ -26,4 +26,37 @@ WHERE
   customer_id = @customer_id
   OR @customer_id IS NULL
 
+-- Wyœwietl filmy, które znajduj¹ siê w wybranym sklepie (store)
+
+declare @store_id int
+set @store_id = 1
+
+ 
+select DISTINCT
+	film.title, 	
+	store.store_id
+from store
+	inner join inventory as i
+		on store.store_id = i.store_id
+	inner join film
+		on film.film_id = i.film_id
+	where 
+		store.store_id = @store_id
+
+-- Wyœwietl identyfikatory, datê p³atnoœci i wartoœæ dla wybranego okresu
+-- | payment_id | payment_date | amount |
+
+declare @from_payment_date datetime
+declare @to_payment_date datetime
+
+set @from_payment_date = '2005-07-01'
+set @to_payment_date ='2005-07-06'
+
+select
+	payment_id,
+	payment_date,
+	amount
+from payment
+where payment_date BETWEEN @from_payment_date AND @to_payment_date
+order by amount desc
 
