541ebc67a615c956675f9123df2bf6e4f53f9e69
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-08 14:03:58 +0100

    Dodanie podzapytań

--- sql/podzapytania.sql
+++ sql/podzapytania.sql
@@ -1,1 +1,14 @@
 
+-- Podzapytania
+
+SELECT TOP(10) * FROM (
+
+	SELECT title FROM film AS f
+		INNER JOIN inventory AS i
+			ON i.film_id = f.film_id
+		WHERE i.store_id = 2
+	EXCEPT 
+	SELECT title FROM film AS f
+		INNER JOIN inventory AS i
+			ON i.film_id = f.film_id
+		WHERE i.store_id = 1 ) AS unique_films
