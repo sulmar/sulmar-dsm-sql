1ebc2b2c2fa181c2e5e3ca29e58f664ec228cb46
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-03-08 14:04:22 +0100

    Dodanie rozwiązania

--- sql/zbiory.sql
+++ sql/zbiory.sql
@@ -51,7 +51,47 @@ CROSS JOIN customer
 
 -- Wyœwietl tytu³y filmów, które znajduj¹ siê obu wypo¿yczalniach (store). Ile jest takich filmów?
 
+SELECT film_id FROM film
+EXCEPT SELECT distinct f.film_id FROM film AS f
+INNER JOIN
+       inventory AS i
+              ON f.film_id = i.film_id
+INNER JOIN
+rental AS r
+
+ON i.inventory_id = r.inventory_id
+
+SELECT film_id FROM film
+EXCEPT 
+SELECT i.film_id 
+FROM inventory AS i
+	INNER JOIN rental AS r
+	ON i.inventory_id = r.inventory_id
+
+SELECT * from rental 
 
 
 
+SELECT film_id, title FROM film
+EXCEPT SELECT distinct f.film_id, f.title 
+FROM film AS f
+INNER JOIN
+       inventory AS i
+              ON f.film_id = i.film_id
+INNER JOIN
+rental AS r
 
+ON i.inventory_id = r.inventory_id
+
+-- Wyœwietl tytu³y filmów, które znajduj¹ siê tylko w drugiej wypo¿yczalni (store). Ile jest takich filmów?
+
+SELECT title FROM film AS f
+	INNER JOIN inventory AS i
+		ON i.film_id = f.film_id
+	WHERE i.store_id = 2
+EXCEPT 
+SELECT title FROM film AS f
+	INNER JOIN inventory AS i
+		ON i.film_id = f.film_id
+	WHERE i.store_id = 1
+	
